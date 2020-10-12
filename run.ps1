9468c82c5022c6e77627eb907483a5d5957d1082
sadicLiu <190978752@qq.com>
2020-10-12 17:07:39 +0800

    add windows auto run script

--- run.ps1
+++ run.ps1
@@ -1,3 +1,14 @@
-cd cmake-build-debug
+rm -r -fo cmake-build-debug
 
-./run.exe pred
+mkdir cmake-build-debug
+cd cmake-build-debug
+cp ../military.flv ./
+cp ../tank.jpg ./
+
+cmake -G"Unix Makefiles" ..
+make -j
+
+cd ..
+
+./cmake-build-debug/run demo
+
