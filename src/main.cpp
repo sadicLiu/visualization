2086e459bc14096e3531f8d08f21f8590d05737d
sadicLiu <190978752@qq.com>
2020-10-12 17:20:34 +0800

    add main function of this demo

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,50 @@
 
 using namespace std;
 
+// 对视频文件进行分类并展示
 void run_predict()
 {
     cout << "run pred..." << endl;
 
     string video_path = "./military.flv";
-    process_video(video_path);
+
+    VideoCapture capture(video_path);
+    Mat frame;
+
+    while (true)
+    {
+        if (capture.read(frame))
+        {
+            resize(frame, frame, Size(640, 480));
+
+            // float *pred = predict(network, img);  // todo: invoke network prediction here, 224x224 input
+            float *pred = predict();
+            show_result(frame, pred, TOPK, false, false);
+            delete[]pred;
+        } else
+        {
+            break;
+        }
+    }
 }
 
+// 对测试集进行分类, 并打印分类准确率
 void run_test()
 {
     cout << "run test..." << endl;
+}
 
+// 分类一张图片的demo
+void run_demo()
+{
+    string img_path = "./tank.jpg";
+    Mat img = imread(img_path);
+    resize(img, img, Size(640, 480));
+
+    // float *pred = predict(network, img);  // todo: invoke network prediction here, 224x224 input
+    float *pred = predict();
+    show_result(img, pred, TOPK, true, true);
+    delete[]pred;
 }
 
 
@@ -31,7 +63,7 @@ int main(int argc, char **argv)
 
     if (argc < 2)
     {
-        fprintf(stderr, "usage: %s <pred> | <test>\n", argv[0]);
+        fprintf(stderr, "usage: %s <pred> | <test> | <demo>\n", argv[0]);
         return 0;
     }
 
@@ -41,19 +73,13 @@ int main(int argc, char **argv)
     } else if (0 == strcmp(argv[1], "test"))
     {
         run_test();
+    } else if (0 == strcmp(argv[1], "demo"))
+    {
+        run_demo();
     } else
     {
         fprintf(stderr, "Not an option: %s\n", argv[1]);
     }
-
-
-    // process video
-//    string video_path = "../military.flv";
-//    process_video(video_path);
-
-    // process single image
-//    string img_path = "../tank.jpg";
-//    process_img(img_path, true, true);
 
     return 0;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,52 @@
 #ifndef DEMO_UTILS_H
 #define DEMO_UTILS_H
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+
 void strip_args(char *s);
+
+// 实现argsort功能
+template<typename T>
+vector<int> argsort(const vector<T> &array)
+{
+    const int array_len(array.size());
+    vector<int> array_index(array_len, 0);
+    for (int i = 0; i < array_len; ++i)
+        array_index[i] = i;
+
+    sort(array_index.begin(), array_index.end(),
+         [&array](int pos1, int pos2)
+         { return (array[pos1] > array[pos2]); });
+
+    return array_index;
+}
+
+
+template<typename _Tp>
+int softmax(const _Tp *src, _Tp *dst, int length)
+{
+    const _Tp alpha = *std::max_element(src, src + length);
+    _Tp denominator{0};
+
+    for (int i = 0; i < length; ++i)
+    {
+        dst[i] = std::exp(src[i] - alpha);
+        denominator += dst[i];
+    }
+
+    for (int i = 0; i < length; ++i)
+    {
+        dst[i] /= denominator;
+    }
+
+    return 0;
+}
+
 
 #endif //DEMO_UTILS_H
